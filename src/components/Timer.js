c00fa9cdae64ac6b6dc03dc1203185d34696ab18
andrevinicius201 <andre.vinicius1899@gmail.com>
2024-03-18 16:39:04 -0300

    control restrictions modal

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,5 @@
 import { Component, useEffect, createRef } from "react";
+import WarningModal from "./warningModal";
 
 
 export default class Timer extends Component {
@@ -11,6 +12,8 @@ export default class Timer extends Component {
         this.pauseTimer=this.pauseTimer.bind(this);
         this.tick=this.tick.bind(this);
         this.updateRequestedMinutes=this.updateRequestedMinutes.bind(this);
+        this.handleShowModalStatus=this.handleShowModalStatus.bind(this);
+        this.refreshPage=this.refreshPage.bind(this);
         this.state = {
             minutes: "00",
             seconds: 0,
@@ -18,11 +21,20 @@ export default class Timer extends Component {
             runnnig: false,
             initiated: false,
             buttonMessage: "Start timer",
-            disableSetupFieldButton: false
+            disableSetupFieldButton: false,
+            showModal: false,
+            modalMessage: "Testando"
         };
         this.audioButtonReference = createRef();
         this.timeOverAudioReference = createRef();
         
+    }
+
+    handleShowModalStatus(status, msg) {
+        this.setState({
+            showModal:status,
+            modalMessage:msg
+        })
     }
 
     initAudio = () => {
@@ -44,7 +56,13 @@ export default class Timer extends Component {
 
     controlTimer(){
         if(!this.state.initiated){
-            this.startTimer()
+            if(this.state.minutes > 0){
+                this.startTimer()
+            } else {
+                let msg="You need to set up the desired time before proceed"
+                this.handleShowModalStatus(true, msg)
+            }
+            
         }
 
         if(this.state.initiated && this.state.runnnig){
@@ -117,24 +135,34 @@ export default class Timer extends Component {
     }
 
     updateRequestedMinutes(operation){
+
         this.initAudio()
-        if(operation == "plus"){
+        if(operation == "plus" && !this.state.initiated){
             this.setState({
                 minutes: this.zeroPad(parseInt(this.state.minutes) + 5, 2)
             })
-        } else {
-            if (this.state.minutes > 0) {
+        } else if (this.state.minutes > 0 && !this.state.initiated) {
                 this.setState({
                     minutes: this.zeroPad(parseInt(this.state.minutes) - 5, 2)
-                })
-            } 
+            })
+        } else {
+            this.setState({
+                showModal:true
+            })
         }
+        
     }
 
-    
+    refreshPage() {
+        window.location.reload();
+    }
+
 
 
     render() {
+
+        const message = "testando"
+
       return (
             <div className="bg-red-700 w-full h-screen flex flex-col justify-center items-center m-auto">
                 
@@ -154,12 +182,31 @@ export default class Timer extends Component {
                     </div>
 
                 
-                <button onClick={this.controlTimer} className="m-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-                    {
-                        this.state.buttonMessage
-                    }
-                </button>
 
+                
+                    <button onClick={this.controlTimer} className="w-3/6 mt-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded md:w-2/6 lg:w-2/6 xl:w-1/6 2xl:w-1/6">
+                        {
+                            this.state.buttonMessage
+                        }
+                    </button>
+                    <button onClick={this.refreshPage} className="w-3/6 mt-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded md:w-2/6 lg:w-2/6 xl:w-1/6 2xl:w-1/6">
+                        {/* <div className="flex flex-wrap"> */}
+                            
+                            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-1/12 h-1/5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                            </svg> */}
+
+                            <span>
+                                Restart timer
+                            </span>
+                            
+                        {/* </div> */}
+  
+                    </button>
+                
+
+
+                {this.state.showModal && <WarningModal sendDataToParent={this.handleShowModalStatus} message={this.state.modalMessage}/>}
                 
             </div>
                                 
